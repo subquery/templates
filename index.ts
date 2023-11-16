7ebf7409b9834eba1361fc841896141ff4f4548b
James Bayly <46693720+jamesbayly@users.noreply.github.com>
2023-11-16 09:53:22 +0800

    Concordium mainnet (#26)
    * Concordium mainnet
    
    * [SKIP CI] update output
    
    * Correct Concordium Mainnet
    
    * [SKIP CI] update output
    
    ---------
    
    Co-authored-by: jamesbayly <jamesbayly@users.noreply.github.com>

--- index.ts
+++ index.ts
@@ -1652,6 +1652,24 @@ const networkFamilies: NetworkFamily[] = [
     logo: "https://static.subquery.network/network-logos/concordium.png",
     networks: [
       {
+        code: "9dd9ca4d19e9393877d2c44b70f89acbfc0883c2243e5eeaecc0d1cd0503f478",
+        name: "Concordium",
+        chain_id:
+          "9dd9ca4d19e9393877d2c44b70f89acbfc0883c2243e5eeaecc0d1cd0503f478",
+        description: "",
+        logo: "https://static.subquery.network/network-logos/concordium.png",
+        examples: [
+          {
+            name: "concordium-starter",
+            description:
+              "This SubQuery project indexes all transfer transactions, updated transaction events, and block rewards on the Concordium Network",
+            remote: "https://github.com/subquery/concordium-subql-starter",
+            path: "Concordium/concordium-starter",
+          },
+        ],
+        guides: [],
+      },
+      {
         code: "4221332d34e1694168c2a0c0b3fd0f273809612cb13d000d5c2e00e85f50f796",
         name: "Concordium Testnet",
         chain_id:
