c120792f9b2dcaab518f968ef09d6cf868eaf745
yoozo-mac <304738717@qq.com>
2024-09-14 17:02:06 +0800

    subsquid util

--- utils/subsquid-discover.ts
+++ utils/subsquid-discover.ts
@@ -1,4 +1,27 @@
 import { apiGet } from './request';
 
 const SUBSQUID_GATEWAY_PATH = 'https://cdn.subsquid.io/archives/evm.json';
+interface Provider {
+    data: string[];
+    dataSourceUrl: string;
+    provider: string;
+    release: string;
+    supportTier: number;
+}
 
+interface Archive {
+    chainId: number;
+    chainName: string;
+    isTestnet?: boolean;
+    network: string;
+    providers: Provider[];
+}
+
+interface ArchivesResponse {
+    archives: Archive[];
+}
+
+apiGet(SUBSQUID_GATEWAY_PATH).then((data) => {
+
+    console.log(data)
+})
